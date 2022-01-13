f4c1d41b15c48fcb6c8071ef3f4fdb13f178c2ba
Harsha-0611 <komaraharshavardhan.19.it@anits.edu.in>
2022-01-14 00:26:00 +0530

    hello

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}))
 
+app.listen(process.env.PORT || 3000,function(){
+  console.log("server running");
+})
+
 app.get("/",function(req,res){
 res.sendFile(__dirname+"/index.html")
 })
@@ -27,7 +31,4 @@ https.get(url,function(response){
   })
 })
 
-})
-app.listen(process.env.PORT || 3000,function(){
-  console.log("server running");
 })
