eafcdcc6c9eb28eea2601dcf7ad59c8e9807cb84
Harsha-0611 <komaraharshavardhan.19.it@anits.edu.in>
2022-01-13 23:59:57 +0530

    changed index.js

--- index.js
+++ index.js
@@ -17,10 +17,13 @@ var url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ae111c
 https.get(url,function(response){
   response.on("data",function(data){
     const weatherdetails=JSON.parse(data);
-    res.write("<h1>Weather in "+city+" is:       "+weatherdetails.main.temp+" degree celsius</h1>")
-    res.write("<h3>weather condition :"+weatherdetails.weather[0].description+"</h3>")
-    res.write("<img src='http://openweathermap.org/img/wn/"+weatherdetails.weather[0].icon+"@2x.png'>")
-    res.send()
+    a1="<h1>Weather in "+city+" is:       "+weatherdetails.main.temp+" degree celsius</h1>"
+    // res.write("<h1>Weather in "+city+" is:       "+weatherdetails.main.temp+" degree celsius</h1>")
+    a2="<h3>weather condition :"+weatherdetails.weather[0].description+"</h3>"
+    // res.write("<h3>weather condition :"+weatherdetails.weather[0].description+"</h3>")
+    a3="<img src='http://openweathermap.org/img/wn/"+weatherdetails.weather[0].icon+"@2x.png'>"
+    // res.write("<img src='http://openweathermap.org/img/wn/"+weatherdetails.weather[0].icon+"@2x.png'>")
+    res.send(a1+a2+a3)
   })
 })
 
